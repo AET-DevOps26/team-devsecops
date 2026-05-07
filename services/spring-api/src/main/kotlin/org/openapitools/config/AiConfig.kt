a9e8226c491a71eb079575ca4f4b8f7ff28e7d0a
Jakob Schödl <jakob.schoedl@tum.de>
2026-05-07 17:44:48 +0200

    fix: shorten server pooling interval

--- services/spring-api/src/main/kotlin/org/openapitools/config/AiConfig.kt
+++ services/spring-api/src/main/kotlin/org/openapitools/config/AiConfig.kt
@@ -2,14 +2,26 @@ package org.openapitools.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
+import java.time.Duration
 
 @Configuration
 class AiConfig {
 	@Bean
-	fun aiWebClient(): WebClient =
-		WebClient
+	fun aiWebClient(): WebClient {
+		val pool = ConnectionProvider.builder("ai")
+			// maxIdleTime must stay below uvicorn's --timeout-keep-alive (default 5s). Otherwise, it sometimes happens
+			// that we try to reuse an existing connection because a new request comes in within >5s, but uvicorn
+			// already discarded the connection.
+			.maxIdleTime(Duration.ofSeconds(2))
+			.build()
+		return WebClient
 			.builder()
 			.baseUrl("http://localhost:8081")
+			.clientConnector(ReactorClientHttpConnector(HttpClient.create(pool)))
 			.build()
+	}
 }
